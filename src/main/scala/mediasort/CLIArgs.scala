7026f803c1018628d1ded14c91366cd641a3378a
Andrew Roberts <adroberts@gmail.com>
2020-01-05 09:02:03 -0500

    basic global cli arguments

--- src/main/scala/mediasort/CLIArgs.scala
+++ src/main/scala/mediasort/CLIArgs.scala
@@ -1,7 +1,26 @@
 package mediasort
 
+import java.io.File
+
 import org.rogach.scallop._
 
 class CLIArgs(arguments: Seq[String]) extends ScallopConf(arguments) {
+  version(s"mediasort v${Option(getClass.getPackage.getImplementationVersion).getOrElse("dev")}")
+  banner(
+    """Usage: mediasort [OPTION [OPTION ...]] <COMMAND> [COMMAND-ARGS]
+      |Act on filesystem paths based on rules
+      |Options:
+      |""".stripMargin
+  )
+
+  // TODO: require config path only when running a subcommand
+  val configPath = opt[File]("config", descr = "path to config.yml")
+  val dryRun = opt[Boolean]("dry-run", descr = "don't make any filesystem changes")
+  val quiet = opt[Boolean]("quiet", descr = "less logging")
+  val verbose = opt[Boolean]("verbose", descr = "more logging")
+
+  validateFileIsFile(configPath)
+  mutuallyExclusive(quiet, verbose)
+
   verify
 }
