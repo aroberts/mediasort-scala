3d60f39ee1dcc1be88381428f20010be8a6f3aac
Andrew Roberts <adroberts@gmail.com>
2020-01-05 18:33:47 -0500

    restructured cli args without subcommands for simplicity- there were never enough use cases for subcommands

--- src/main/scala/mediasort/CLIArgs.scala
+++ src/main/scala/mediasort/CLIArgs.scala
@@ -7,20 +7,28 @@ import org.rogach.scallop._
 class CLIArgs(arguments: Seq[String]) extends ScallopConf(arguments) {
   version(s"mediasort v${Option(getClass.getPackage.getImplementationVersion).getOrElse("dev")}")
   banner(
-    """Usage: mediasort [OPTION [OPTION ...]] <COMMAND> [COMMAND-ARGS]
-      |Act on filesystem paths based on rules
+    """Act on filesystem paths based on rules
+      |
+      |Usage: mediasort [OPTION [OPTION ...]] <path>
+      |
       |Options:
       |""".stripMargin
   )
 
-  // TODO: require config path only when running a subcommand
-  val configPath = opt[File]("config", descr = "path to config.yml")
+  val configPath = opt[File]("config", descr = "path to config.yml", argName = "path", required = true)
   val dryRun = opt[Boolean]("dry-run", descr = "don't make any filesystem changes")
   val quiet = opt[Boolean]("quiet", descr = "less logging")
   val verbose = opt[Boolean]("verbose", descr = "more logging")
+  val validateCfg = opt[Boolean]("validate", descr = "validate config and exit")
+  val path = trailArg[File]("path", descr = "path to act on", required = false)
 
   validateFileIsFile(configPath)
   mutuallyExclusive(quiet, verbose)
+  validateOpt(validateCfg, path) {
+    case (Some(false), None) => Left("must provide a path to act on")
+    case _ => Right(())
+  }
 
   verify
 }
+
