15b8354c6c80ff4248968d99af33164d392116da
Andrew Roberts <adroberts@gmail.com>
2020-01-09 15:19:17 -0500

    wrapper for caching mime types

--- src/main/scala/mediasort/classify/MimeType.scala
+++ src/main/scala/mediasort/classify/MimeType.scala
@@ -1,12 +1,17 @@
 package mediasort.classify
 
 import os.Path
-
 import javax.activation
+import mediasort.paths
 
 object MimeType {
   val impl = new activation.MimetypesFileTypeMap()
 
   def apply(s: String): String = impl.getContentType(s)
   def apply(s: Path): String = apply(s.last)
+
+  case class MimedPath(p: Path, mimeType: String)
+  def mimedPaths(in: Path) =
+    paths.expandFiles(in).map(p => MimedPath(p, apply(p)))
+
 }
