e7559cc84d8303fe6e006223af7e804944d337b4
Andrew Roberts <adroberts@gmail.com>
2020-01-19 21:50:30 -0500

    made matcher with score native-regex

--- src/main/scala/mediasort/classify/MatcherWithScore.scala
+++ src/main/scala/mediasort/classify/MatcherWithScore.scala
@@ -4,8 +4,14 @@ import io.circe.Decoder
 import mediasort.config.Config.jsonCfg
 import io.circe.generic.extras.semiauto._
 
-case class MatcherWithScore(pattern: String, score: Int)
+import scala.util.Try
+import scala.util.matching.Regex
+
+case class MatcherWithScore(pattern: Regex, score: Int, titleGroup: Option[Int]) {
+
+}
 object MatcherWithScore {
+  implicit val decodeRegex: Decoder[Regex] = Decoder[String].emapTry(s => Try(s.r))
   implicit val decodeMatcherWithScore: Decoder[MatcherWithScore] = deriveConfiguredDecoder
 }
 
