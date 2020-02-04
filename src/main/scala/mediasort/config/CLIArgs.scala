08e7b479fe2848dba4d91cfff4dd56bb8bb34236
Andrew Roberts <adroberts@gmail.com>
2020-02-04 10:25:54 -0500

    added fully configurable logging

--- src/main/scala/mediasort/config/CLIArgs.scala
+++ src/main/scala/mediasort/config/CLIArgs.scala
@@ -13,16 +13,18 @@ object CLIArgs {
   val config = Opts.option[Path]("config", help = "path to config.yml", short = "c", metavar = "path")
   val log = Opts.option[Path]("log", help = "log here as well as stdout", short = "l", metavar = "path").orNone
   val dryRun = Opts.flag("dry-run", help = "log actions instead of performing them", short = "d").orFalse
-  val quiet = Opts.flag("quiet", help = "less logging", short = "q").orFalse
-  val verbose = Opts.flag("verbose", help = "more logging", short = "v").orFalse
+  val quiet = Opts.flags("quiet", help = "less logging", short = "q").withDefault(0)
+  val verbose = Opts.flags("verbose", help = "more logging", short = "v").withDefault(0)
   val version = Opts.flag("version", help = "print version and exit", visibility = Visibility.Partial)
   val path = Opts.argument[Path]("input path")
 
   val logLevel: Opts[Level] = (verbose, quiet).tupled.mapValidated {
-    case (true, false) => Validated.valid(Level.Debug)
-    case (false, false) => Validated.valid(Level.Info)
-    case (false, true) => Validated.valid(Level.Warn)
-    case _ => Validated.invalidNel(s"Can't pass both $quiet and $verbose")
+    case (0, 0) => Validated.valid(Level.Info)
+    case (1, 0) => Validated.valid(Level.Debug)
+    case (_, 0) => Validated.valid(Level.Trace)
+    case (0, 1) => Validated.valid(Level.Warn)
+    case (0, _) => Validated.valid(Level.Error)
+    case _ => Validated.invalidNel("Can't pass both --quiet and --verbose")
   }
 
   val parser = Command("mediasort", "Act on filesystem paths based on rules") {
