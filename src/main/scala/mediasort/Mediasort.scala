c5a62eec4c766c6415e0f53784e2f87b538193a7
Andrew Roberts <adroberts@gmail.com>
2021-06-04 21:25:03 -0400

    refactored and consolidated the main event loop to support watch more gracefully

--- src/main/scala/mediasort/Mediasort.scala
+++ src/main/scala/mediasort/Mediasort.scala
@@ -5,6 +5,7 @@ import mediasort.config.{CLIArgs, Config}
 import mediasort.clients.{Clients, Logging}
 import cats.effect._
 import cats.syntax.show._
+import cats.syntax.either._
 import mediasort.errors._
 import fs2.Stream
 import fs2.io.Watcher.Event
@@ -18,48 +19,45 @@ object Mediasort extends IOApp {
 
   def version = Option(getClass.getPackage.getImplementationVersion).getOrElse("dev")
 
-  def inputPathProgram(input: Path, args: CLIArgs): Stream[IO, Unit] = for {
-    cfg <- Config.load[Config](args.configPath)
-    clients <- Clients.fromConfig(cfg)
-    _ <- processInput(input, args.dryRun, cfg, clients)
-  } yield ()
-
-  def watchDirProgram(watch: Path, args: CLIArgs): Stream[IO, Unit] = for {
+  def program(args: CLIArgs): Stream[IO, Unit] = for {
     cfg <- Config.load[Config](args.configPath)
     clients <- Clients.fromConfig(cfg)
 
-    _ = scribe.info(s"Watching $watch for files with input paths...")
+    _ = args.input.swap.toOption.map(d => scribe.info(s"Watching $d for files with input paths..."))
 
-    event <- paths.watch(watch, Created, Deleted)
-    input <- watchForInputs(event).handleErrorWith(e => Stream.eval(errorHandler(e, 3)))
+    _ <- args.input match {
+      case Left(watchDir) =>
+        (for {
+          // TODO: can we detect the completion of the processing of an "event" and delete the file?
+          //  add --deleteWatch flag to remove watch files after processing them
+          //  - or do it by default- --no-delete-watch
+          //  delete watchfiles unless an error occurs during processing or --no-delete-watch
+          event <- paths.watch(watchDir, Created)
+          inputPath <- watchForPaths(event)
+          _ <- processPath(inputPath, args.dryRun, cfg, clients)
 
-    // need to "attempt" these steps
-    // - input creation (can fail)
-    // - processInput() call
-
-    // basically everything should be attempted rather than allowed to bubble in "watch mode"
-
-    // the two Program functions could be collapsed if
-    // - the watch could be conditionally applied
-    // - the "outer" (app level) error handling can be unified
-    //   - i think it can; we want the same basic error functionality if any of the "outer" code
-    //   errors
+          // treat watch-generated inputs as their own "mini run" of the program, trapping any
+          // errors generated. Otherwise, an error during processing a watch input would bubble up
+          // to the main event loop and halt the watch stream.
+        } yield ()).handleErrorWith(e => Stream.eval(errorHandler(e, 3)))
+      case Right(inputPath) => processPath(inputPath, args.dryRun, cfg, clients)
+    }
   } yield ()
 
-  def watchForInputs(e: Event) = e match {
+  def watchForPaths(e: Event) = e match {
     case Event.Created(path, count) =>
       Stream.emit(IO(scribe.debug(s"checking $path for inputs..."))) >>
         paths.readFile(path)
           .flatMap(contents => Stream.emits(contents.split("\\\\n")))
           .map(_.trim)
           .filter(_.nonEmpty)
-          .evalTap(s => IO(scribe.trace(s" - constructing Input from $s")))
-          .evalMap(s => Input(Paths.get(s)))
+          .evalTap(s => IO(scribe.debug(s" - constructing Input from $s")))
+          .map(s => Paths.get(s))
 
-    case x => Stream.emit(IO(println(s" ignoring $x"))) >> Stream.empty
+    case x => Stream.emit(IO(scribe.trace(s" ignoring $x"))) >> Stream.empty
   }
 
-  def processInput(input: Path, dryRun: Boolean, cfg: Config, clients: Clients) = for {
+  def processPath(input: Path, dryRun: Boolean, cfg: Config, clients: Clients) = for {
     input <- Stream.eval(Input(input))
     _ = scribe.info(s"$input")
 
@@ -83,20 +81,11 @@ object Mediasort extends IOApp {
         case Right(parsed) =>
           Logging.configure(parsed.logLevel, parsed.logPath)
 
-          parsed.input match {
-            case Left(watch) => watchDirProgram(watch, parsed)
-              .compile
-              .drain
-              .as(ExitCode.Success)
-              // TODO: errors will be different in watch mode
-//              .handleErrorWith(e => errorHandler(e, 3).as(ExitCode.Error))
-
-            case Right(input) => inputPathProgram(input, parsed)
-              .compile
-              .drain
-              .as(ExitCode.Success)
-              .handleErrorWith(e => errorHandler(e, 3).as(ExitCode.Error))
-          }
+          program(parsed)
+            .compile
+            .drain
+            .as(ExitCode.Success)
+            .handleErrorWith(e => errorHandler(e, 3).as(ExitCode.Error))
       }
     }
   }
@@ -104,7 +93,7 @@ object Mediasort extends IOApp {
   // TODO: this should be tailrec-able
   def errorHandler(e: Throwable, depth: Int): IO[Unit] =
     IO(scribe.error("[Global] " + e.show)) *>
-      IO(e.getStackTrace.foreach(s => scribe.trace(s.toString))) *>
+      IO(scribe.trace("[Global]", e)) *>
       (e match {
         case ex: Exception if depth > 0 && Option(ex.getCause).isDefined => errorHandler(ex.getCause, depth - 1)
         case _ => IO.pure(())
