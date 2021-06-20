b4aeb8c7e813ed2d20d98147bd6dfad52095e6a3
Andrew Roberts <adroberts@gmail.com>
2021-06-20 14:26:17 -0400

    turn off outside logging unless -vv

--- src/main/scala/mediasort/clients/Logging.scala
+++ src/main/scala/mediasort/clients/Logging.scala
@@ -1,17 +1,24 @@
 package mediasort.clients
 
 import java.nio.file.Path
-
-import scribe.Level
+import scribe.{Level, LogRecord, Priority}
 import scribe.format._
 import scribe.handler.LogHandler
+import scribe.modify.LogModifier
 import scribe.writer.FileWriter
 
 object Logging {
 
+  def silenceOtherLoggers: LogModifier = new LogModifier {
+    override def priority = Priority.High
+    override def apply[M](record: LogRecord[M]) =
+      if (record.className.startsWith("mediasort")) Some(record) else None
+  }
+
   def configure(logLevel: Level, logPath: Option[Path]): Unit = {
     val logFormat = formatter"$date $level $message"
-    val console = LogHandler(formatter = logFormat, minimumLevel = Some(logLevel))
+    val modifiers = if (logLevel == Level.Trace) Nil else silenceOtherLoggers :: Nil
+    val console = LogHandler(formatter = logFormat, minimumLevel = Some(logLevel), modifiers = modifiers)
     val file = logPath.map(p => console.withWriter(FileWriter().path(_ => p).append))
 
     List(Some(console), file).flatten
